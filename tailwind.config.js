039420d00b81a2fe73dbf0da13cfc2def208ca7a
Ivis <ivismarino91@gmail.com>
2024-04-22 11:34:45 -0400

    feat(index): adding styling and hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      screens: {
+        sm: "320px",
+        xl: "1440px",
+      },
+    },
+    extend: {
+      colors: {
+        Green: "hsl(75, 94%, 57%)",
+        White: "hsl(0, 0%, 100%)",
+        grey: "hsl(0, 0%, 20%)",
+        "dark-Grey": "hsl(0, 0%, 12%)",
+        "off-Black": "hsl(0, 0%, 8%)",
+      },
+      fontFamily: {
+        "inter-bold": ["Inter-Bold", "sans-serif"],
+        "inter-semibold": ["Inter-Semibold", "sans-serif"],
+        "inter-regular": ["Inter-Regular", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
